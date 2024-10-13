b61f23093ab06d929cc49d9083be79420dddec81
niccostantini <65295160+niccostantini@users.noreply.github.com>
2024-10-13 18:24:44 +0200

    Add deleteToDoById function

--- src/index.js
+++ src/index.js
@@ -6,46 +6,103 @@ function generateID() {
 }
 
 // ToDo object
-const ToDo = function(label, priority, dueDate, id = "") {
+const ToDo = function(label, priority, dueDate, id) {
     this.id = id;
     this.label = label;
     this.priority = priority;
     this.dueDate = dueDate;
 };
 
-// Project  object
-const Project = function (title, toDos = []) {
-    this.id = generateID(); //This ID will be assigned to each child ToDo so that it will be easier to fetch
+// Project object
+const Project = function (id, title, toDos = []) {
+    this.id = id;
     this.title = title;
-    this.toDos = toDos;
+    this.toDos = toDos.map(toDo => new ToDo(toDo.label, toDo.priority, toDo.dueDate, toDo.id));
 
-    this.addToDo = function (label, priority, dueDate) {
-        const newToDo = new ToDo(label, priority, dueDate, this.id /** ID set as the same as its parent project's */);
+    this.addToDo = function (label, priority, dueDate, id) {
+        const newToDo = new ToDo(label, priority, dueDate, id);
         this.toDos.push(newToDo);
     };
 };
 
 // Project creation
 function createProject(title, toDos = []) {
-    const newProject = new Project(title, toDos);
-    localStorage.setItem(newProject.id, JSON.stringify(newProject));
-    return newProject;
-};
+    const newProject = new Project(generateID(), title, toDos);
+    console.log("Generated Project ID:", newProject.id);
 
-//Project fetch function
-/**  It returns the project as an object */
+    localStorage.setItem(newProject.id, JSON.stringify(newProject));
+
+    console.log("Stored Project ID:", newProject.id);
+
+    return newProject;
+}
+
+// Project fetch function
 function fetchProjectById(id) {
-    const projectData = localStorage.getItem(id); //get project based on ID
+    const projectData = localStorage.getItem(id);
     if (projectData) {
         const projectObj = JSON.parse(projectData);
-        return new Project(projectObj.title, projectObj.toDos);
-    }
-    else { /**In case the ID is not found, handle the error */
+        return new Project(projectObj.id, projectObj.title, projectObj.toDos);
+    } else {
         console.error("NO SUCH PROJECT EXISTS");
         return null;
     }
 }
 
-console.log("CAZZO")
+function addToDo(label, priority, dueDate, projectId) {
+    const project = fetchProjectById(projectId);
+    if (project) {
+        project.addToDo(label, priority, dueDate, generateID()); //Set random ID for each task
+        localStorage.setItem(projectId, JSON.stringify(project));
+    }
+}
 
-console.table(fetchProjectById("x1hsfsnnt"))
+function deleteProjectById(id) {
+    const project = fetchProjectById(id);
+    if (project) {
+        localStorage.removeItem(id);
+        console.log(`Deleted project with ID ${id}`);
+    } else {
+        console.error("NO SUCH PROJECT EXISTS");
+    }
+}
+
+function deleteToDoById(toDoId) {
+    const keys = Object.keys(localStorage);
+    for (let key of keys) {
+        const project = fetchProjectById(key);
+        if (project) {
+            const toDoIndex = project.toDos.findIndex(toDo => toDo.id === toDoId);
+            if (toDoIndex !== -1) {
+                project.toDos.splice(toDoIndex, 1);
+                localStorage.setItem(project.id, JSON.stringify(project));
+                console.log(`Deleted ToDo with ID ${toDoId} from project with ID ${project.id}`);
+                return;
+            }
+        }
+    }
+    console.error(`NO SUCH TODO EXISTS with ID ${toDoId}`);
+}
+
+
+localStorage.clear();
+var prova = createProject("PorcoDio");
+console.log(`Created project with ID ${prova.id} and title ${prova.title}`);
+
+console.table(fetchProjectById(prova.id));
+console.log(`Fetched project with ID ${prova.id}`);
+
+addToDo("Nutri il gatto", "high", "ieri", prova.id);
+console.log(`Added task to project with ID ${prova.id}`);
+
+console.table(fetchProjectById(prova.id));
+
+addToDo("Nuota tre volte", "high", "ieri", prova.id);
+
+console.table(fetchProjectById(prova.id));
+
+deleteToDoById(fetchProjectById(prova.id).toDos[1].id)
+
+// deleteProjectById(prova.id);
+
+// fetchProjectById(prova.id);
